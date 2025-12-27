000d8a51ac3c9c91aa29699cc3aa4b96fba6d342
Rastim Safarov <rast875@gmail.com>
2025-12-27 11:55:22 +0000

    4. Integrating React and configuring CSS in Webpack

---
+++ src/components/Counter.tsx
@@ -0,0 +1,15 @@
+import {useState} from "react";
+import './counter.scss'
+
+export const Counter = () => {
+    const [count, setCount] = useState(0)
+    const increment = () => setCount(count + 1)
+    const decrement = () => setCount(count - 1)
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={increment}>+</button>
+            <button onClick={decrement}>-</button>
+        </div>
+    )
+}
