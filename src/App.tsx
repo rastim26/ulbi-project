7726457e993b759184402236e3ed46107ff265a4
Rastim Safarov <rast875@gmail.com>
2025-12-28 00:19:27 +0000

    7. Organizing styles. Adding theme support

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import './index.scss'
-import {Routes, Route, Link} from "react-router-dom";
-import AboutPage from "./pages/AboutPage/AboutPage";
-import MainPage from "./pages/MainPage/MainPage";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {Suspense} from "react";
+import './styles/index.scss'
+import {Routes, Route, Link} from "react-router-dom"
+import {MainPageAsync} from "./pages/MainPage/MainPage.async"
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async"
+import {Suspense} from "react"
+import {useTheme} from "./theme/useTheme";
+
+
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme()
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>{theme}</button>
             <Link to="/">Main</Link>
             <Link to="/about">About</Link>
             <Suspense fallback={<p>Loading...</p>}>
