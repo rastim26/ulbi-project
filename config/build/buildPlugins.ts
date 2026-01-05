ad8c605f69741aba0520b145cbf7d8ce613f499b
Rastim Safarov <rast875@gmail.com>
2026-01-05 22:43:42 +0000

    16. Setup Webpack hot module replacement plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -17,5 +17,6 @@ export function buildPlugins(options: BuildOptions): webpack.WebpackPluginInstan
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
+        new webpack.HotModuleReplacementPlugin(),
     ]
 }
