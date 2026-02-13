4df46d32c2f0e1107c883d12d5f796c4e2011ec6
Rastim Safarov <rast875@gmail.com>
2026-02-13 08:24:23 +0000

    23 Add bundle size analysis using BundleAnalyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import HtmlWebpackPlugin from 'html-webpack-plugin'
 import webpack from 'webpack'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 import { BuildOptions } from './types/config'
 
 export function buildPlugins(options: BuildOptions): webpack.WebpackPluginInstance[] {
@@ -20,5 +21,8 @@ export function buildPlugins(options: BuildOptions): webpack.WebpackPluginInstan
         }),
         new ReactRefreshPlugin(),
         new webpack.HotModuleReplacementPlugin(),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }),
     ]
 }
