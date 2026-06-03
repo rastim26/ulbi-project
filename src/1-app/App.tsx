01449d4190171705c51b6b7bf6b21d2d2930a569
Rastim Safarov <rast875@gmail.com>
2026-06-03 08:40:43 +0100

    Introduce centralized palette, replace old variable names

--- src/1-app/App.tsx
+++ src/1-app/App.tsx
@@ -4,13 +4,13 @@ import { useTheme } from '1-app/providers/ThemeProvider'
 import { AppRouter } from '1-app/providers/router'
 import { Navbar } from '3-widgets/Navbar'
 import { Sidebar } from '3-widgets/Sidebar'
-import { clsx } from '6-shared/lib/clsx/clsx'
 import { Loader } from '6-shared/ui/Loader/Loader'
 
 const App = () => {
     const { theme } = useTheme()
+
     return (
-        <div className={clsx('app', theme)}>
+        <div data-theme={theme} className="app">
             <Suspense fallback={<Loader />}>
                 <Navbar />
                 <div className="content-page">
